3f14278ea6ff619e8adf53ae5776789fea163ed6
Janaina <janascal@gmail.com>
2020-02-09 19:38:45 -0300

    Add Windowns Dialog and Button UI

--- Creational/Factory/Java/Main.java
+++ Creational/Factory/Java/Main.java
@@ -1,9 +1,11 @@
 import gui.Button;
 import gui.Dialog;
+import gui.WindownsDialog;
 
 class Main {
     public static void main(String[] args) {
-        Dialog dialog = new Dialog();
+        Dialog dialog = new WindownsDialog();
+        dialog.render();
         Button b = dialog.createButton();
         b.onClick();
     }
