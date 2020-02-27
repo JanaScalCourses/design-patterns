7f38f2ed61ca341c8597b24001feaf9e72222dc9
Janaina Scal Duia Castello <janascal@gmail.com>
2020-02-27 10:17:52 -0300

    Improve main example

--- Creational/Prototype/Java/src/college/College.java
+++ Creational/Prototype/Java/src/college/College.java
@@ -5,6 +5,11 @@ import college.Classroom.ClassroomLesson;
 import college.Distance.DistanceCourse;
 import college.Distance.DistanceLesson;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class College {
   private static void enroll(Course course, Student student) {
     course.enroll(student);
@@ -12,40 +17,46 @@ public class College {
   }
 
   public static void main(String[] args) {
+    Map<Semester, List<Course>> coursesPerSemester = new HashMap<>();
+    List<Course> courses = new ArrayList<>();
     Semester semester = Semester.create(2020, 1);
+    coursesPerSemester.put(semester, courses);
+
     Student jana = new Student("Janaina");
 
     ClassroomCourse classroomCourse = new ClassroomCourse("Design Patterns", semester, "Isaac");
-    ClassroomLesson classroomLesson = new ClassroomLesson("Design Patterns content");
-    classroomCourse.addLesson(classroomLesson);
+    courses.add(classroomCourse);
+    classroomCourse.addLesson(new ClassroomLesson("Design Patterns content"));
     classroomCourse.createTest("some question");
 
     DistanceCourse distanceCourse = new DistanceCourse("Java", semester, "Google");
-    DistanceLesson distanceLesson = new DistanceLesson("Java content");
-    distanceCourse.addLesson(distanceLesson);
+    courses.add(distanceCourse);
+    distanceCourse.addLesson(new DistanceLesson("Java content"));
     distanceCourse.createTest("some project");
 
-    enroll(classroomCourse, jana);
-    classroomCourse.attend(0, jana);
-    enroll(distanceCourse, jana);
-    distanceCourse.attend(0, jana);
-
-    classroomCourse.applyTest(0);
-    distanceCourse.applyTest(0);
+    courses.forEach(course -> {
+      enroll(course, jana);
+      course.attend(0, jana);
+      course.applyTest(0);
+    });
 
     jana.showDetails();
-    System.out.println();
-    classroomCourse.showDetails();
-    System.out.println();
-    distanceCourse.showDetails();
 
-    System.out.println("\n-------------- New Semester --------------\n");
     try {
-      DistanceCourse newDistanceCurse = distanceCourse.newSemester();
-      newDistanceCurse.showDetails();
-      System.out.println();
-      ClassroomCourse newClassroomCourse = classroomCourse.newSemester();
-      newClassroomCourse.showDetails();
+      courses = new ArrayList<>();
+      for (Course course : coursesPerSemester.get(semester)) {
+        courses.add(course.newSemester());
+      }
+      Semester nextSemester = courses.get(0).getSemester();
+      coursesPerSemester.put(nextSemester, courses);
+
+      coursesPerSemester.forEach((sem, cours) -> {
+        System.out.println("\n-------------- New Semester --------------\n");
+        for (Course course : cours) {
+          course.showDetails();
+          System.out.println();
+        }
+      });
 
     } catch (CloneNotSupportedException e) {
       e.printStackTrace();
